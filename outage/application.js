c0a82f4632ebd2f1ac2bfc8f36626292d647d104
nonowarn <nonowarn@gmail.com>
2011-03-14 12:26:52 +0900

    Add pagination

--- outage/application.js
+++ outage/application.js
@@ -5,34 +5,58 @@ jQuery(function($) {
     3: "12:20～16:00",
     4: "13:50～17:30",
     5: "15:20～19:00"
-  };
+  },
+    itemsPerPage = 20;
 
-  var $timingTable = $("#timings"),
+  var found = Timings;
+
+  var $timingTable = $("table#timings tbody"),
       timingTableDom = $timingTable.get(0),
-      $search = $("#search").submit(filterRow);
+      $search = $("#search").submit(filterRow),
+      $pagination = $("#pagination")
 
   function init() {
-    buildTable();
+    initPagination();
   }
 
-  function buildTable() {
-    setTimeout(function() {
-      timingTableDom.innerHTML += TimingTableHTML;
-    }, 0);
+  function updateTimings(pageNumber) {
+    var itemsHtml = "";
+    $timingTable.find("tr.timing").remove();
+    for (var i = pageNumber * itemsPerPage, l = i + itemsPerPage; i < l; ++i) {
+      if (found[i]) {
+        itemsHtml += createRow(found[i]);
+      }
+    }
+    timingTableDom.innerHTML += itemsHtml;
+  }
+
+  function createRow(timing) {
+    return '<tr class="timing"><td>'
+      + timing.town
+      + '</td><td>'
+      + Slot[timing.group]
+      + '</td></tr>';
+  }
+
+  function initPagination() {
+    $pagination.pagination(found.length, {
+      items_per_page: itemsPerPage,
+      callback: updateTimings
+    });
+    $("#pagination").trigger('setPage', [0]);
   }
 
   function filterRow() {
-    var query = $(this).serializeArray()[0].value,
-        $timingTableRows = $timingTable.find("tr:has(td)");
-    $timingTableRows.each(function() {
-      var $this = $(this);
-      if ($this.find("td:first").text().indexOf(query) == -1) {
-        this.className = "hidden";
-      } else {
-        this.className = "show";
-      }
-    });
+    var query = $(this).serializeArray()[0].value;
+    found = Timings.filter(townLike(query));
+    initPagination();
     return false;
+  }
+
+  function townLike(query) {
+    return function(timing) {
+      return timing.town.indexOf(query) !== -1;
+    }
   }
 
   init();
--- outage/build_timings.js.rb
+++ outage/build_timings.js.rb
@@ -1,25 +1,10 @@
 # -*- coding: utf-8 -*-
 require 'json'
 
-slot = [
-  nil,
-  "06:20～10:00, 16:50～20:30",
-  "09:20～13:00, 18:20～22:00",
-  "12:20～16:00",
-  "13:50～17:30",
-  "15:20～19:00"
-]
-timings = {}
-
+timings = []
 IO.readlines("timings.csv").each do |line|
-  pref, city, town, zone = line.chomp.split(/,/)
-  timings[pref + city + town] ||= slot[zone.to_i]
+  pref, city, town, group = line.chomp.split(/,/)
+  timings << { town: pref + city + town, group: group.to_i }
 end
-
-print 'var TimingTableHTML = "'
-timings.each do |city, slot|
-  print <<-HTML
-    <tr><td>#{city}</td><td>#{slot}</td></tr> \\
-  HTML
-end
-print '"'
+print 'var Timings ='
+print timings.to_json
