1c2bc5bd74ce7b87359a1f6962f3c58a0cea7db7
nonowarn <nonowarn@gmail.com>
2011-03-17 09:09:16 +0900

    Refactor

--- outage/application.js
+++ outage/application.js
@@ -1,16 +1,18 @@
 jQuery(function($) {
-  var BaseSlot = {
-    3: "06:20～10:00",
-    4: "09:20～13:00",
-    5: "12:20～16:00",
-    1: "15:20～19:00",
-    2: "18:20～22:00"
-  },
-      Slot = BaseSlot;
-      itemsPerPage = 20;
+  var ItemsPerPage = 20,
+      DateFormat = "yy/mm/dd",
+      BaseSlot = {
+        3: "06:20～10:00",
+        4: "09:20～13:00",
+        5: "12:20～16:00",
+        1: "15:20～19:00",
+        2: "18:20～22:00"
+      },
+      NumGroup = 5,
+      BaseDate = $.datepicker.parseDate(DateFormat, "2011/03/15");
 
-  var Timings = flatten(AllTimings),
-      DateFormat = "yy/mm/dd";
+  var currentTimings = flatten(AllTimings),
+      currentSlot = BaseSlot;
 
   var $timingTable = $("table#timings tbody"),
       timingTableDom = $timingTable.get(0),
@@ -18,7 +20,7 @@ jQuery(function($) {
       $pagination = $("#pagination"),
       $date = $("#date");
 
-  var found = Timings;
+  var found = currentTimings;
 
   function init() {
     initSlot();
@@ -26,10 +28,37 @@ jQuery(function($) {
     initDatepicker();
   }
 
+  function initPagination() {
+    refreshPage();
+  }
+
+  function initDatepicker() {
+    $date
+      .datepicker()
+      .val($.datepicker.formatDate(DateFormat, new Date))
+      .change(updateGroup);
+  }
+
+  function initSlot() {
+    var today = new Date;
+    updateSlot(today);
+  }
+
+  function refreshPage() {
+    $pagination.pagination(found.length, {
+      items_per_page: ItemsPerPage,
+      callback: updateTimings,
+      num_display_entries: 0,
+      next_text: "結果の続きを見る",
+      prev_text: "さっきの結果に戻る"
+    });
+    $("#pagination").trigger('setPage', [0]);
+  }
+
   function updateTimings(pageNumber) {
     var itemsHtml = "";
     $timingTable.find("tr.timing").remove();
-    for (var i = pageNumber * itemsPerPage, l = i + itemsPerPage; i < l; ++i) {
+    for (var i = pageNumber * ItemsPerPage, l = i + ItemsPerPage; i < l; ++i) {
       if (found[i]) {
         itemsHtml += createRow(found[i]);
       }
@@ -45,53 +74,37 @@ jQuery(function($) {
       + '</td></tr>';
   }
 
-  function initPagination() {
-    $pagination.pagination(found.length, {
-      items_per_page: itemsPerPage,
-      callback: updateTimings,
-      num_display_entries: 0,
-      next_text: "結果の続きを見る",
-      prev_text: "さっきの結果に戻る"
-    });
-    $("#pagination").trigger('setPage', [0]);
+  function timeString(group) {
+    return currentSlot[group];
   }
 
-  function initDatepicker() {
-    $date
-      .datepicker()
-      .val($.datepicker.formatDate(DateFormat, new Date))
-      .change(updateGroup);
+  function updateSlot(date) {
+    currentSlot = slotAt(date);
   }
 
-  function initSlot() {
-    Slot = slotAt(new Date);
+  function updateGroup() {
+    var $this = $(this),
+        thisDate = $.datepicker.parseDate(DateFormat, $this.val());
+    updateSlot(thisDate);
+    refreshPage();
   }
 
   function slotAt(date) {
-    var baseDate = $.datepicker.parseDate(DateFormat, "2011/03/15"),
-        difference = dateDiff(date, baseDate),
-        numGroup = 5,
+    var difference = dateDiff(date, BaseDate),
         newSlot = {};
 
     for (var i in BaseSlot) {
-      var newI = (Number(i) + difference) % numGroup || 5;
+      var newI = (Number(i) + difference) % NumGroup || 5;
       newSlot[newI] = BaseSlot[i];
     }
 
     return newSlot;
   }
 
-  function updateGroup() {
-    var $this = $(this),
-        thisDate = $.datepicker.parseDate(DateFormat, $this.val());
-    Slot = slotAt(thisDate);
-    initPagination();
-  }
-
-
   function dateDiff(lhs, rhs) {
     return Math.floor((lhs.getTime() - rhs.getTime()) / (1000*60*60*24));
   }
+
   function flatten(timings) {
     var r = [];
 
@@ -107,10 +120,14 @@ jQuery(function($) {
     return r;
   }
 
+  function updateFoundItem(query) {
+    found = currentTimings.filter(townLike(query));
+  }
+
   function filterRow() {
     var query = $(this).serializeArray()[0].value.replace(/\s+/, "");
-    found = Timings.filter(townLike(query));
-    initPagination();
+    updateFoundItem(query);
+    refreshPage();
     return false;
   }
 
@@ -118,10 +135,6 @@ jQuery(function($) {
     return function(timing) {
       return timing.town.indexOf(query) !== -1;
     }
-  }
-
-  function timeString(group) {
-    return Slot[group];
   }
 
   init();
